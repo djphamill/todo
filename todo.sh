3b31913557ded10388f727768a7116587a3a0dfa
David <djphamill@gmail.com>
2024-04-06 19:09:45 +0100

    Add ability to pass in simple arg.

--- todo.sh
+++ todo.sh
@@ -2,10 +2,18 @@
 
 TODO_FILEPATH=$YOUR_FILEPATH
 
-echo "to-do:"
-LINE_NUMBER=0
+case "$1" in
+	"add")
+		echo "add an item";;
 
-while read p; do
-	((LINE_NUMBER++))
-	echo "$LINE_NUMBER: $p"
-done <"$TODO_FILEPATH"
+	"list")
+		echo "to-do:"
+		LINE_NUMBER=0
+		while read p; do
+			((LINE_NUMBER++))
+			echo "$LINE_NUMBER: $p"
+		done <"$TODO_FILEPATH";;
+
+	"delete")
+		echo "delete an item";;
+esac
